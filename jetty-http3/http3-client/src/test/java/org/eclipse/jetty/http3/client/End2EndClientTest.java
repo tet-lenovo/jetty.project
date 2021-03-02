f630b8969a28182c4781a1990312909c788c403b
Ludovic Orban <lorban@bitronix.be>
2021-03-02 09:30:42 +0100

    reset server code in test
    Signed-off-by: Ludovic Orban <lorban@bitronix.be>

--- jetty-http3/http3-client/src/test/java/org/eclipse/jetty/http3/client/End2EndClientTest.java
+++ jetty-http3/http3-client/src/test/java/org/eclipse/jetty/http3/client/End2EndClientTest.java
@@ -27,49 +27,49 @@ import org.junit.jupiter.api.Test;
 
 public class End2EndClientTest
 {
-//
-//    private Server server;
-//
-//    @BeforeEach
-//    void setUp() throws Exception
-//    {
-//        server = new Server();
-//
-//        SSLKeyPair keyPair = new SSLKeyPair(new File("src/test/resources/keystore.p12"), "PKCS12", "storepwd".toCharArray(), "mykey", "storepwd".toCharArray());
-//        QuicConnector quicConnector = new QuicConnector(server);
-//        quicConnector.setPort(8443);
-//        quicConnector.setKeyPair(keyPair);
-//        server.setConnectors(new Connector[]{quicConnector});
-//
-//        HttpConfiguration config = new HttpConfiguration();
-//        config.setHttpCompliance(HttpCompliance.LEGACY); // enable HTTP/0.9
-//        HttpConnectionFactory factory = new HttpConnectionFactory(config);
-//        quicConnector.addConnectionFactory(factory);
-//
-//        HandlerList handlers = new HandlerList();
-//        handlers.setHandlers(new Handler[]{new AbstractHandler() {
-//            @Override
-//            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
-//            {
-//                baseRequest.setHandled(true);
-//                PrintWriter writer = response.getWriter();
-//                writer.println("<html>\n" +
-//                    "\t<body>\n" +
-//                    "\t\tRequest served\n" +
-//                    "\t</body>\n" +
-//                    "</html>");
-//            }
-//        }});
-//        server.setHandler(handlers);
-//
-//        server.start();
-//    }
-//
-//    @AfterEach
-//    void tearDown() throws Exception
-//    {
-//        server.stop();
-//    }
+
+    private Server server;
+
+    @BeforeEach
+    void setUp() throws Exception
+    {
+        server = new Server();
+
+        SSLKeyPair keyPair = new SSLKeyPair(new File("src/test/resources/keystore.p12"), "PKCS12", "storepwd".toCharArray(), "mykey", "storepwd".toCharArray());
+        QuicConnector quicConnector = new QuicConnector(server);
+        quicConnector.setPort(8443);
+        quicConnector.setKeyPair(keyPair);
+        server.setConnectors(new Connector[]{quicConnector});
+
+        HttpConfiguration config = new HttpConfiguration();
+        config.setHttpCompliance(HttpCompliance.LEGACY); // enable HTTP/0.9
+        HttpConnectionFactory factory = new HttpConnectionFactory(config);
+        quicConnector.addConnectionFactory(factory);
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{new AbstractHandler() {
+            @Override
+            public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+            {
+                baseRequest.setHandled(true);
+                PrintWriter writer = response.getWriter();
+                writer.println("<html>\n" +
+                    "\t<body>\n" +
+                    "\t\tRequest served\n" +
+                    "\t</body>\n" +
+                    "</html>");
+            }
+        }});
+        server.setHandler(handlers);
+
+        server.start();
+    }
+
+    @AfterEach
+    void tearDown() throws Exception
+    {
+        server.stop();
+    }
 
     @Test
     void name() throws Exception
